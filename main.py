a78ab0cc9653749c6533dc5a969d5883133afb07
Gonsalsa Laptop <sebgon02@gmail.com>
2026-05-19 18:42:12 +0200

    test commit

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
+
 
 app = FastAPI()
 
 #To start the api, write this in the terminal:
 # uvicorn main:app --reload
 
+
+class User(BaseModel):
+    name: str
+    email: EmailStr
+    account_id: int
+
+user = User(name="sebastian", email="sebastian@email.com", account_id=1)
+
+print(user)
+
 @app.get("/hello")
 def hello():
-    return "Hello World"
+    return user
